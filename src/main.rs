3f221ce9c1f68f3774491c2b15eb5af94e343fb1
Putra Nugraha <putnug1122@gmail.com>
2023-07-19 21:51:59 +0800

    add compile checking and alter functionality

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ struct Todo {
     id: i32,
     title: String,
     done: bool,
+    description: Option<String>,
 }
 
 #[tokio::main]
@@ -20,6 +21,13 @@ async fn main() {
         .await
         .expect("Failed to connect to the database");
 
+    // STARTS WITH CLEAN TABLE
+    sqlx::query("TRUNCATE TABLE todos RESTART IDENTITY")
+        .execute(&pool)
+        .await
+        .expect("Failed to truncate table!");
+
+    // INSERT ON ROW
     let first_todo_title = "Learn SQLx";
     sqlx::query("INSERT INTO todos (title) VALUES ($1)")
         .bind(first_todo_title)
@@ -27,6 +35,7 @@ async fn main() {
         .await
         .unwrap();
 
+    // GET ROW BY IT'S TITLE
     let first_todo = sqlx::query_as::<_, Todo>("SELECT * FROM todos WHERE title=$1")
         .bind(first_todo_title)
         .fetch_one(&pool)
@@ -36,5 +45,26 @@ async fn main() {
     println!(
         "query_as: id={}, title={}, done={:?}",
         first_todo.id, first_todo.title, first_todo.done
+    );
+
+    // QUERY WITH COMPILE TIME CHECKING
+    let second_todo_title = "Give feedback to this blog post";
+    sqlx::query!("INSERT INTO todos (title) VALUES ($1)", second_todo_title)
+        .execute(&pool)
+        .await
+        .unwrap();
+
+    let second_todo = sqlx::query_as!(
+        Todo,
+        "SELECT * FROM todos WHERE title=$1",
+        second_todo_title
+    )
+    .fetch_one(&pool)
+    .await
+    .unwrap();
+
+    println!(
+        "query_as: id={}, title={}, done={:?}",
+        second_todo.id, second_todo.title, second_todo.done
     );
 }
